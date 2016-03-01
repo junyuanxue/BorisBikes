72c2dd325b86c8a1dee7eebd188ab6f29eb3b98e
Junyuan Xue <junyuanxue@Junyuans-MacBook-Air.local>
2016-03-01 23:13:37 +0000

    Raised errors when docking station is empty/full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,11 +5,13 @@ class DockingStation
   attr_reader:bike
 
   def dock(bike)
+    raise "Docking Station full" if @bike
     @bike = bike
   end
 
   def release_bike
-    Bike.new
+    raise 'No bikes available' unless @bike
+    @bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,31 @@
-require 'docking_station'
+require "docking_station"
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :bike}
-
   it { is_expected.to respond_to :release_bike }
+  describe "#release_bike" do
+    it "raises an error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
 
-  it 'releases bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it "releases bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument}
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  describe "#dock" do
+    it "raises an error when the Docking Station is full" do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error "Docking Station full"
+    end
+  end
 
-  it 'returns docked bikes' do
+  it { is_expected.to respond_to :bike }
+
+  it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
