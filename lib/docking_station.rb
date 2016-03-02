72a8d59f4f2becd5acef4e35950c57913cf38553
Junyuan Xue <junyuanxue@Junyuans-MacBook-Air.local>
2016-03-02 15:57:28 +0000

    Replaced magic numbers with constants

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,8 @@ require_relative "bike"
 
 class DockingStation
   attr_reader :bikes
+  DEFAULT_CAPACITY = 20
+
   def initialize
     @bikes = []
   end
@@ -20,7 +22,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.size >= 20
+    @bikes.size >= DEFAULT_CAPACITY
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require "docking_station"
 
 describe DockingStation do
+
     it { is_expected.to respond_to(:release_bike) }
     describe "release_bike" do
       it "releases bike" do
@@ -28,7 +29,7 @@ describe DockingStation do
       end
 
       it "raises an error" do
-        20.times { subject.dock(Bike.new) }
+        DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
         expect { subject.dock(Bike.new) }.to raise_error "The docking station is full"
       end
     end
