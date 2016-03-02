b913c89e3b15aaf9ec95f7bdc9616c78dc6e4aa0
Claudia Green <claudia@claudiag.me.uk>
2016-03-02 15:43:25 +0000

    Introduced private emmpty? & full? methods. Refactored.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,14 +7,24 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no bikes left" if @bikes.empty?
+    raise "There are no bikes left" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "The docking station is full" if @bikes.size >= 20
+    raise "The docking station is full" if full?
     @bikes << bike
     bike
+  end
+
+  private
+
+  def full?
+    @bikes.size >= 20
+  end
+
+  def empty?
+    @bikes.size == 0
   end
 
 end
