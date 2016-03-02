0d51255f5b12914b9b04c573cf4d9de710bb0919
Claudia Green <claudia@claudiag.me.uk>
2016-03-02 11:07:37 +0000

    Tests for 'bike properly docked' added. Tests passing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,21 @@ require 'bike'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike)}
-
     it 'expects bikes to be working' do
       bike = subject.release_bike
       expect(bike).to be_working
     end
 
+    it { is_expected.to respond_to(:dock).with(1).argument}
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'bike properly docked' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
 
 end
